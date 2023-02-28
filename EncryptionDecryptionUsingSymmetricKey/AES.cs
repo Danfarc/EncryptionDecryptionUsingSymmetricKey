92d66ad562d74f8e6e074b5ee32bcb14abcbd157
dfarana <dfarana@alumina.com>
2023-02-28 16:08:26 -0500

    Agregar archivos de proyecto.

---
+++ EncryptionDecryptionUsingSymmetricKey/AES.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionDecryptionUsingSymmetricKey
+{
+    public static class AES
+    {
+        private static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes;
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new())
+            {
+                using var AES = Aes.Create("AesManaged"); // This reports Warning  SYSLIB0022  'RijndaelManaged' is obsolete: 'The Rijndael and RijndaelManaged types are obsolete. Use Aes instead.
+                AES.KeySize = 256;
+                AES.BlockSize = 128;
+                var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                AES.Key = key.GetBytes(AES.KeySize / 8);
+                AES.IV = key.GetBytes(AES.BlockSize / 8);
+                AES.Mode = CipherMode.CBC;
+                using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                    cs.Close();
+                }
+                encryptedBytes = ms.ToArray();
+            }
+            return encryptedBytes;
+        }
+
+        private static byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            using (MemoryStream ms = new())
+            {
+                using var AES = Aes.Create("AesManaged"); // This reports Warning  SYSLIB0022  'RijndaelManaged' is obsolete: 'The Rijndael and RijndaelManaged types are obsolete. Use Aes instead.
+                AES.KeySize = 256;
+                AES.BlockSize = 128;
+                var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                AES.Key = key.GetBytes(AES.KeySize / 8);
+                AES.IV = key.GetBytes(AES.BlockSize / 8);
+                AES.Mode = CipherMode.CBC;
+                using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                    cs.Close();
+                }
+                decryptedBytes = ms.ToArray();
+            }
+            return decryptedBytes;
+        }
+
+        public static string EncryptText(string password, string salt = "MySecretSaltWhichIWantToKeepWorking")
+        {
+            byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(password);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(salt);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+            string result = Convert.ToBase64String(bytesEncrypted);
+            return result;
+        }
+
+        public static string DecryptText(string hash, string salt = "MySecretSaltWhichIWantToKeepWorking")
+        {
+            try
+            {
+                byte[] bytesToBeDecrypted = Convert.FromBase64String(hash);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(salt);
+                passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+                byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+                string result = Encoding.UTF8.GetString(bytesDecrypted);
+                return result;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        private const int SALT_BYTE_SIZE = 24;
+        private const int HASH_BYTE_SIZE = 24;
+        private const int PBKDF2_ITERATIONS = 1000;
+        private const int ITERATION_INDEX = 0;
+        private const int SALT_INDEX = 1;
+        private const int PBKDF2_INDEX = 2;
+
+        public static string PBKDF2_CreateHash(string password)
+        {
+            //RNGCryptoServiceProvider csprng = new(); // This reports SYSLIB0023 'RNGCryptoServiceProvider' is obsolete: 'RNGCryptoServiceProvider is obsolete. To generate a random number, use one of the RandomNumberGenerator static methods instead.'
+            var csprng = RandomNumberGenerator.Create();
+            byte[] salt = new byte[SALT_BYTE_SIZE];
+            csprng.GetBytes(salt);
+            byte[] hash = PBKDF2(password, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
+            return PBKDF2_ITERATIONS + ":" + Convert.ToBase64String(salt) + ":" + Convert.ToBase64String(hash);
+        }
+
+        public static bool PBKDF2_ValidatePassword(string password, string correctHash)
+        {
+            char[] delimiter = { ':' };
+            string[] split = correctHash.Split(delimiter);
+            int iterations = Int32.Parse(split[ITERATION_INDEX]);
+            byte[] salt = Convert.FromBase64String(split[SALT_INDEX]);
+            byte[] hash = Convert.FromBase64String(split[PBKDF2_INDEX]);
+            byte[] testHash = PBKDF2(password, salt, iterations, hash.Length);
+            return SlowEquals(hash, testHash);
+        }
+
+        private static bool SlowEquals(byte[] a, byte[] b)
+        {
+            uint diff = (uint)a.Length ^ (uint)b.Length;
+            for (int i = 0; i < a.Length && i < b.Length; i++)
+                diff |= (uint)(a[i] ^ b[i]);
+            return diff == 0;
+        }
+
+        private static byte[] PBKDF2(string password, byte[] salt, int iterations, int outputBytes)
+        {
+            Rfc2898DeriveBytes pbkdf2 = new(password, salt)
+            {
+                IterationCount = iterations
+            };
+            return pbkdf2.GetBytes(outputBytes);
+        }
+    }
+}
---
+++ EncryptionDecryptionUsingSymmetricKey/Program.cs
@@ -0,0 +1,47 @@
+﻿using EncryptionDecryptionUsingSymmetricKey;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var key = "b14ca5898a4e4133bbce2ea2315a1916";
+        //var key = "P455W0rd";
+
+        //Console.WriteLine("Please enter a secret key for the symmetric algorithm.");
+        //var key = Console.ReadLine();
+
+        Console.WriteLine("Please enter a string for encryption");
+        var str = Console.ReadLine();
+        var encryptedString = AesOperation.EncryptString(key, str);
+        Console.WriteLine($"AesOperation encrypted string = {encryptedString}");
+
+        var decryptedString = AesOperation.DecryptString(key, encryptedString);
+        Console.WriteLine($"AesOperation decrypted string = {decryptedString}");
+
+        var securityManagerEncryp = SecurityManager.Encrypt(str);
+        Console.WriteLine($"SecurityManager encrypted string = {securityManagerEncryp}");
+
+        var securityManagerDecryp = SecurityManager.Decrypt(securityManagerEncryp);
+        Console.WriteLine($"SecurityManager decrypted string = {securityManagerDecryp}");
+
+        var securityManagerEncryp1 = AESEncryption.Encrypt(str,key);
+        Console.WriteLine($"AESEncryption encrypted string = {securityManagerEncryp1}");
+
+        var securityManagerDecryp1 = AESEncryption.Decrypt(securityManagerEncryp1, key);
+        Console.WriteLine($"AESEncryption decrypted string = {securityManagerDecryp1}");
+
+        var securityManagerEncryp2 = AES.EncryptText(str, key);
+        Console.WriteLine($"AES encrypted string = {securityManagerEncryp2}");
+
+        var securityManagerDecryp2 = AES.DecryptText(securityManagerEncryp2, key);
+        Console.WriteLine($"AES decrypted string = {securityManagerDecryp2}");
+
+        var securityManagerEncryp3 = AES.PBKDF2_CreateHash(str);
+        Console.WriteLine($"AES encrypted string = {securityManagerEncryp3}");
+
+        var securityManagerDecryp3 = AES.PBKDF2_ValidatePassword(str, securityManagerEncryp3);
+        Console.WriteLine($"AES decrypted string = {securityManagerDecryp3}");
+
+        Console.ReadKey();
+    }
+}
